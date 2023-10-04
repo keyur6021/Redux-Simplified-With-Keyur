11103c1182fab07dd68e6362f3258b9720ef4dc9
keyur6021 <mulanikeyur90@gmail.com>
2023-10-04 23:49:46 +0530

    add some route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
