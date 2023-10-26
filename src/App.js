55f6fe7ca222377210fad7da58c18e4d90d43d85
keyur6021 <mulanikeyur90@gmail.com>
2023-10-26 21:15:22 +0530

    fix the minor changes of product form

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import Home from './components/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ListDownPage from './components/ListDownPage';
-import AddProductForm from './components/FileForm';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import { Toaster } from 'react-hot-toast';
+import AddProductForm from './components/ProductForm';
 
 
 
